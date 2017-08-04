f0af54d06445476b243efd8bfddbb49a19dc99f7
wpnmax <wpnmax@hotmail.com>
2017-08-04 20:23:28 +0300

    Добавил объект и заставил его двигаться

---
+++ TestProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 5f;
+
+    void Start()
+    {
+		
+    }
+	
+
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
+    }
+}
